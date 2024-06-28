a083d6011b4cc78831d6fea2006726ffc2537e4a
Benjamin Bradley <bgcb11@gmail.com>
2024-06-28 18:07:01 -0500

    feat: basic form submission is working; misc fixes

--- sign-up.js
+++ sign-up.js
@@ -10,6 +10,9 @@ userIsCampLeadCheckBox.addEventListener("change", () => {
     yourContactInfoFields.style.display = "none";
   } else {
     yourContactInfoFields.style.display = "block";
+    document.querySelector("#contact-name").value = document.querySelector("#lead-name").value;
+    document.querySelector("#contact-phone-number").value = document.querySelector("#lead-phone-number").value;
+    document.querySelector("#contact-email").value = document.querySelector("#lead-email").value;
   }
 });
 
@@ -69,8 +72,8 @@ function showEventSelector(events) {
 }
 
 async function getCurrentEvents() {
-  const endpointPath = '/events/current';
-  return fetch(apiBaseUrl + endpointPath)
+  const endpointPath = `${apiBaseUrl}/events/current`;
+  return fetch(endpointPath)
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -87,8 +90,42 @@ async function getCurrentEvents() {
   });
 }
 
-function submitForm() {
-  alert('submitting form')
+function submitForm(event) {
+  event.preventDefault();
+  const event_id = document.querySelector("#event_id").textContent;
+  const endpointPath = `${apiBaseUrl}/events/${event_id}/regs`;
+  // TODO: basic validation on client-side - required values, etc. ?
+  const formData = new FormData();
+  formData.append('lead_name', document.querySelector('#lead-name').value);
+  formData.append('lead_phone', document.querySelector('#lead-phone-number').value);
+  formData.append('lead_email', document.querySelector('#lead-phone-number').value); // Note: This seems to be a mistake in the HTML. The email input should have its own ID.
+  formData.append('contact_name', document.querySelector('#contact-name').value);
+  formData.append('contact_phone', document.querySelector('#contact-phone-number').value);
+  formData.append('contact_email', document.querySelector('#contact-email').value);
+  formData.append('camp_name', document.querySelector('#camp-name').value);
+  formData.append('desired_number', document.querySelector('#desired-number').value);
+  formData.append('desired_callerid', document.querySelector('#desired-caller-id').value);
+  formData.append('own_phone', document.querySelector('input[name="phone-to-use"]:checked').value);
+  formData.append('message', document.querySelector('#notes').value);
+  fetch(endpointPath, {
+    method: 'POST',
+    body: formData,
+  })
+  // TODO: improve error handling
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Form submission successful', data);
+    alert('Form submitted successfully!');
+  })
+  .catch(error => {
+    console.error('There has been a problem with your form submission:', error);
+    alert('Error submitting form');
+  });
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
